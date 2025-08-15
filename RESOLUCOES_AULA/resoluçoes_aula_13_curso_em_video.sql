efc95be3709dd563a5c893614d90db63d1386541
AnaCristina <anacrisribeirost@gmail.com>
2025-08-15 14:32:08 -0300

    atualizaçoes

---
+++ RESOLUCOES_AULA/resoluçoes_aula_13_curso_em_video.sql
@@ -0,0 +1,27 @@
+/* 
+
+Atividades aula 13 - Select (Parte II) - Estudonauta (Gustavo Guanabara) 
+
+*/
+
+-- 1) Selecionar uma lista com todos os gafanhotos do sexo (feminino)
+
+select * from gafanhotos
+where sexo = 'F';
+
+-- 2) Uma lista com os dados de todos aquele sque nascera entre 1/Jan/200 e 31/Dez/2015
+
+-- 3) Uma lista com nome de todos os homens que trabalham como programadores
+
+-- 4) Uma lista de todos os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciado com a letra J. 
+
+-- 5) Uma lista com o nome e nacionalidade de todos os hoemsn que tem Silva no nome, não nasceram no Brasil e pesam menos de 100 KG
+
+-- 6) Qual é a maior altura entre o grupo de gafanhotos que moram no Brasil.
+
+-- 7) Qual é a média de peso entre os gafanhotos (todos) cadastrados?
+
+-- 8) Qual é o menor peso entre as gafanhotas (mulheres) que nasceram fora do Brasil e entre 1/jan/1990 e 31/Dez/2000?
+
+-- 9) Quantas gafanhotos (mulheres) têm amsi de 1.90 de altura?
+
